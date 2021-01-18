a4617af3fc899fac35a7c568b0291f6053e63a18
PatKeenan <patkeenan.dev@gmail.com>
2021-01-18 11:38:42 -0500

    added the main pages for the application, installed bootstrap in the index.html instead of the project itself.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:"/home", pathMatch:"full"},
+
+  {path: '**', redirectTo:"/page-not-found"},
+  {path: 'page-not-found', component: PageNotFoundComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
