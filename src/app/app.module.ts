d5da56f7ae113622bfc07049a8fbda93b3bee0ab
PatKeenan <patkeenan.dev@gmail.com>
2021-01-21 16:28:41 -0500

    removed page not found module from project and redirected everything to the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BeerlistComponent } from './beerlist/beerlist.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
@@ -14,7 +14,6 @@ import { HttpClientModule } from '@angular/common/http'
     AppComponent,
     HomeComponent,
     BeerlistComponent,
-    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
